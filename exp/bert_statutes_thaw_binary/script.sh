e8dbd0c18180661b33d5dee9cefb46b2667b29d5
SgfdDttt <nils.holzenberger@laposte.net>
2020-03-23 18:38:55 -0400

    add scripts for bert_statutes_thaw

---
+++ exp/bert_statutes_thaw_binary/script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+batch=4
+update_period=8
+expdir=exp/bert_statutes_thaw_binary
+warmup=0.1
+learning_rate=5e-05
+date
+CUDA_VISIBLE_DEVICES=`free-gpu` python -u code/train.py --datadir dataset/ --batch $batch --max_length 512 --gpu --expdir $expdir --epochs 100 --warmup $warmup --learning_rate $learning_rate --update_period $update_period --statutes --thaw_top_layer
+date
