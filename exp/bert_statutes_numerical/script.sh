47b8a253095cc9dc41b5c3183d9491bfa3e10385
SgfdDttt <nils.holzenberger@laposte.net>
2020-03-23 18:12:16 -0400

    add scripts for bert_statutes_{binary,numerical}

--- exp/bert_statutes_numerical/script.sh
+++ exp/bert_statutes_numerical/script.sh
@@ -1,8 +1,8 @@
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 batch=4
 update_period=4
-expdir=exp/bert_statutes_binary
-warmup=0.3
+expdir=exp/bert_statutes_numerical
+warmup=0.1
 learning_rate=5e-05
 date
 CUDA_VISIBLE_DEVICES=`free-gpu` python -u code/train.py --datadir dataset/ --batch $batch --max_length 512 --gpu --expdir $expdir --epochs 100 --warmup $warmup --learning_rate $learning_rate --update_period $update_period --statutes
