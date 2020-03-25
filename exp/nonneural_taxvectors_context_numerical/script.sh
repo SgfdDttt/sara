915c9f5bdc75ab114e505262388bf8dc29b483d3
SgfdDttt <nils.holzenberger@laposte.net>
2020-03-25 16:49:08 -0400

    point to tokenized dataset

--- exp/nonneural_taxvectors_context_numerical/script.sh
+++ exp/nonneural_taxvectors_context_numerical/script.sh
@@ -6,5 +6,5 @@ smoothing_parameter=0.001
 weight_decay=0
 word_embeddings=/export/b01/nholzen/tax_law/ablai_tax_vectors.txt
 date
-python -u code/train.py --datadir dataset/ --batch $batch --expdir $expdir --epochs 500 --learning_rate $learning_rate --word_embeddings $word_embeddings --smoothing_parameter $smoothing_parameter --weight_decay $weight_decay --context
+python -u code/train.py --datadir tokenized_dataset/ --batch $batch --expdir $expdir --epochs 500 --learning_rate $learning_rate --word_embeddings $word_embeddings --smoothing_parameter $smoothing_parameter --weight_decay $weight_decay --context
 date
